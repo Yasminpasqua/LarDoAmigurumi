bc4326a713fb416485cca02ce2af96fa176ed492
Yasmin Pereira Pasqua <13771997623@MUZAMBINHO.EDU.BR>
2025-02-03 15:44:54 -0300

    QUASE

--- LarDoAmigurumi/app/urls.py
+++ LarDoAmigurumi/app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import HomeReceitas, LinhasView
+from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', HomeReceitas, name='HomeReceitas'),
     path('linhas/', LinhasView.as_view(), name='linhas'),
+    path('materiais/', MateriaisView.as_view(), name='materiais'),
+    path('receitas/', ReceitasView.as_view(), name='receitas'),
+    path('sobrenos/', SobreNosView.as_view(), name='sobrenos'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- LarDoAmigurumi/app/views.py
+++ LarDoAmigurumi/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Linha
+from .models import *
 
 def HomeReceitas(request):
     return render(request, 'HomeReceitas.html')
@@ -9,3 +9,15 @@ class LinhasView(View):
     def get(self, request, *args, **kwargs):
         linhas = Linha.objects.all()
         return render(request, 'linhas.html', {'linhas': linhas})
+class MateriaisView(View):
+    def get(self, request, *args, **kwargs):
+        materiais = Materiais.objects.all()
+        return render(request, 'materiais.html', {'materiais': materiais})
+class ReceitasView(View):
+    def get(self, request, *args, **kwargs):
+        receita = Receita.objects.all()
+        return render(request, 'receitas.html', {'receita': receita})
+
+class SobreNosView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'sobrenos.html')
